e5f8fa98b15f7c4ae0063c06fcfced2105aabc13
Masayoshi <>
2023-07-02 11:40:29 +0900

    型追加

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,13 @@ hello("Hello World!!");
 // Note: TypeScriptでは型を定義しない場合は「型推論」で型が勝手につく。変数をホバーすることで確認することができる。
 
 ////////////////////////////////////////////
+// string,number,boolean
+////////////////////////////////////////////
+const name: string = "Taro";
+const age: number = 30;
+const isWake: boolean = true;
+
+////////////////////////////////////////////
 // 配列
 ////////////////////////////////////////////
 const arrayNum: number[] = [1, 2, 3, 4, 5];
@@ -57,7 +64,67 @@ let litetalFruits: "Apple" | "Orange" = "Apple";
 let litetalNumber: 0 | 1 = 0;
 
 ////////////////////////////////////////////
-// any (型定義をしない)
+// typeエイリアス（型を変数かする）
+////////////////////////////////////////////
+type ClothSize = "S" | "M" | "L";
+let cloth: ClothSize = "M";
+
+////////////////////////////////////////////
+// any型 (型定義をしない)
 ////////////////////////////////////////////
 //Note: なんでもOK
 const anything: any = 1500;
+
+////////////////////////////////////////////
+// unknown型
+////////////////////////////////////////////
+//Note: any型よりも少し厳しい型。any同様なんでも入れられるが、その変数を使うときに確認が必要。
+let unknownVal: unknown;
+unknownVal = "hello";
+// let text:string = unknownVal; //型エラー
+//↓↓ typeofを使って確認する必要がある
+if (typeof unknownVal === "string") {
+  console.log(unknownVal);
+}
+
+////////////////////////////////////////////
+// never型
+////////////////////////////////////////////
+//Note: 起こり得ない値の型を使うとき。決して何も返さない。
+function error(msg: string): never {
+  throw new Error(msg);
+}
+// console.log(error("This is error!!"));
+
+////////////////////////////////////////////
+// 関数に型を適用
+////////////////////////////////////////////
+//戻り値がある場合
+const add = (a: number, b: number): number => a + b;
+
+//戻り値がない場合(void)
+const cLog = (word: string): void => console.log(word);
+
+//関数の型注釈(arrowになるので注意)
+const add2: (n1: number, n2: number) => number = add;
+
+////////////////////////////////////////////
+// undefined型とnull型
+////////////////////////////////////////////
+//Note: undefined型は値が存在しないことを示す型。滅多に使わない。
+let tmpUndefined: undefined = undefined; //undefinedしか入れれない
+let tmpNull: null = null; //nullしか入れれない
+
+////////////////////////////////////////////
+// callback型
+////////////////////////////////////////////
+// Note: callback部分の型定義はarrowになるので注意
+function cbTest(num: number, callback: (num: number) => number): void {
+  const tmp = callback(num * 2);
+  console.log(tmp);
+}
+const cbTest2 = (num: number): number => {
+  const tmp = num * 3;
+  return tmp;
+};
+cbTest(10, cbTest2);
